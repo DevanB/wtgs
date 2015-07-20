70e7ecb910460ce03e9f001f4792b2ee5d2af079
Devan Beitel <devan.beitel@gmail.com>
2015-07-20 18:24:57 -0500

    Added service-configurable package. Removed useless methods and events to test map markers. Stubbed out function to handle geolocation errors.

--- client/main.js
+++ client/main.js
@@ -1,10 +1,24 @@
 Meteor.startup(function () {
     if ((!Session.get('userLat')) && (!Session.get('userLon'))) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            Session.set('userLat', position.coords.latitude);
-            Session.set('userLon', position.coords.longitude);
-        });
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                Session.set('userLat', position.coords.latitude);
+                Session.set('userLon', position.coords.longitude);
+            }, function (error) {
+                //@TODO: Respond to geolocation error
+            });
+        } else {
+            handleNoGeolocation(true); //@TODO: Write function to prompt for location
+        }
     }
-
     GoogleMaps.load();
 });
+
+function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+        //Geolocation failed.
+    } else {
+        //Browser doesn't support geolocation.
+    }
+
+}
--- client/templates/map.js
+++ client/templates/map.js
@@ -10,25 +10,16 @@ Template.map.onCreated(function () {
             }
         });
 
-
-        google.maps.event.addListener(map.instance, 'click', function (event) {
-            Markers.insert({lat: event.latLng.lat(), lng: event.latLng.lng()});
-        });
-
         var markers = {};
 
         Markers.find().observe({
             added: function (document) {
                 var marker = new google.maps.Marker({
-                    draggable: true,
+                    draggable: false,
                     animation: google.maps.Animation.DROP,
                     position: new google.maps.LatLng(document.lat, document.lng),
                     map: map.instance,
                     id: document._id
-                });
-
-                google.maps.event.addListener(marker, 'dragend', function (event) {
-                    Markers.update(marker.id, {$set: {lat: event.latLng.lat(), lng: event.latLng.lng()}});
                 });
 
                 markers[document._id] = marker;
