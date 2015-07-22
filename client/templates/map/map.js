f742b431cf4b4e2090a5283a993fcd010fc74017
chrisbrightwell <chris@dimwell.net>
2015-07-22 01:48:16 -0500

    adding new map layer

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -21,9 +21,25 @@ Template.map.onRendered(function () {
                 this.map = L.map('map').setView([latitude, longitude], 15);
                 this.map.spin(true);
                 this.mapRendered = true;
-                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+
+                // default maps
+                var default_OpenStreetMaps = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(this.map);
+
+                // optional maps #1
+                var Thunderforest_OpenCycleMap = L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+                	attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                });
+
+                // optional maps #2
+                var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+                  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+                });
+
+                //default_OpenStreetMaps.addTo(this.map);
+                //Thunderforest_OpenCycleMap.addTo(this.map);
+                Esri_WorldTopoMap.addTo(this.map);
 
                 $currentLocation = L.marker([latitude, longitude], {icon: currentLocationType}).addTo(this.map);
                 $currentLocation.bindPopup("<b>Current Location:</b><br>" + latitude + ", " + longitude);
