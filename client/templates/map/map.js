20604f9536e818e1f84815a60f3efb75abae952a
chrisbrightwell <chris@dimwell.net>
2015-07-23 22:48:39 -0500

    add and use the markercluster plugin

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -4,6 +4,7 @@ Template.map.onCreated(function () {
 });
 
 Template.map.onRendered(function () {
+
     var currentLocationType = L.icon({
         iconUrl: 'TrackingDot.png',
         shadowUrl: 'TrackingDot.png',
@@ -18,12 +19,13 @@ Template.map.onRendered(function () {
             latitude = Session.get('location').latitude;
             longitude = Session.get('location').longitude;
             if (!this.mapRendered) {
-                this.map = L.map('map').setView([latitude, longitude], 15);
+                this.map = L.map('map').setView([latitude, longitude], 10);
                 this.map.spin(true);
                 this.mapRendered = true;
-        var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-                  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-                }).addTo(this.map);
+
+                var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+                          attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+                        }).addTo(this.map);
 
                 var geojsonFeature = [ {
                   "type" : "Feature",
@@ -586,14 +588,18 @@ Template.map.onRendered(function () {
                   }
                 }];
 
-                L.geoJson(geojsonFeature, {
+                var geoJsonLayer = L.geoJson(geojsonFeature, {
                   style: function (feature) {
                     return feature.properties.style;
                   },
                   onEachFeature: function (feature, layer) {
                     layer.bindPopup(feature.properties.name + "<br>More Info: <a href=" + feature.properties.popupContent + ">" + feature.properties.popupContent + "</a>");
                   }
-                }).addTo(this.map);
+                });
+
+                var markers = new L.MarkerClusterGroup();
+                markers.addLayer(geoJsonLayer);
+                this.map.addLayer(markers);
 
                 $currentLocation = L.marker([latitude, longitude], {icon: currentLocationType}).addTo(this.map);
                 $currentLocation.bindPopup("<b>Current Location:</b><br>" + latitude + ", " + longitude);
