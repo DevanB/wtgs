36436a1c7f926e39e1c0772313c65cc0ca6b06fc
Devan Beitel <devan.beitel@gmail.com>
2015-07-24 19:16:36 -0500

    Began modifying NOAC splash page.

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -1,77 +1,39 @@
 Template.map.onCreated(function () {
-
     L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
     this.mapRendered = false;
-
-    // if ((!Session.get('userLat')) && (!Session.get('userLon'))) {
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(function (position) {
-    //             Session.set('location', {latitude: position.coords.latitude, longitude: position.coords.longitude});
-    //         }, function (error) {
-    //             handleNoGeolocation(true);
-    //         });
-    //     } else {
-    //         handleNoGeolocation(true);
-    //     }
-    // }
 });
 
 Template.map.onRendered(function () {
-    // var currentLocationType = L.icon({
-    //     iconUrl: 'TrackingDot.png',
-    //     shadowUrl: 'TrackingDot.png',
-    //     iconSize: [21, 23], // size of the icon
-    //     shadowSize: [0, 0], // size of the shadow
-    //     iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
-    //     shadowAnchor: [0, 0],  // the same for the shadow
-    //     popupAnchor: [10, 0] // point from which the popup should open relative to the iconAnchor
-    // });
     this.autorun(function () {
         // if (Session.get('location') && Session.get('location').latitude) {
             // latitude = Session.get('location').latitude;
             // longitude = Session.get('location').longitude;
             if (!this.mapRendered) {
                 this.map = L.map('map').setView([33.246873, -86.784219], 7);
-                this.map.spin(true);
                 this.mapRendered = true;
 
                 var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
                   attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
                 }).addTo(this.map);
-
-                // $currentLocation = L.marker([latitude, longitude], {icon: currentLocationType}).addTo(this.map);
-                // $currentLocation.bindPopup("<b>Current Location:</b><br>" + latitude + ", " + longitude);
-                this.map.spin(false);
+                var geoJsonLayer = L.geoJson($mapData, {
+                  style: function (feature) {
+                    return feature.properties.style;
+                  },
+                  onEachFeature: function (feature, layer) {
+                    layer.bindPopup(feature.properties.name + "<br>More Info: <a href=" + feature.properties.popupContent + ">" + feature.properties.popupContent + "</a>");
+                  }
+                });
+                this.map.addLayer(geoJsonLayer);
+                L.control.locate({
+                  follow: false,
+                  showPopup: true,
+                  locateOptions: {maxZoom: 12}
+                }).addTo(this.map);
                 $('.subscribeModal').modal();
             }
             // if (this.mapRendered) {
-            //     this.map.removeLayer($currentLocation);
-            //     var latitude = Session.get('location').latitude;
-            //     var longitude = Session.get('location').longitude;
-            //     $currentLocation = L.marker([latitude, longitude], {icon: currentLocationType}).addTo(this.map);
-            //     $currentLocation.bindPopup("<b>Current Location:</b><br>" + latitude + ", " + longitude);
             //     this.map.panTo(new L.LatLng(latitude, longitude));
             // }
         // }
     });
 });
-
-// Template.geocodeLocation.events({
-//     'click #searchGeocodeLocationBtn': function(e){
-//         e.preventDefault();
-//         var searchValue = $("#location").val();
-//         var googleGeocodeProvider = new L.GeoSearch.Provider.Google();
-//         googleGeocodeProvider.GetLocations(searchValue, function (data) {
-//             Session.set('location', {latitude: data[0].Y, longitude: data[0].X});
-//         });
-//         //Until session is set, keep searching.
-//     }
-// });
-
-// function handleNoGeolocation(errorFlag) {
-//     if (errorFlag) {
-//         //@TODO: Prompt user to enter zip code or address
-//         // $('#searchModal').modal();
-//         return;
-//     }
-// };
