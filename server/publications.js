e87479eda534ad738777eefa90c794857a37788c
Devan Beitel <devan.beitel@gmail.com>
2015-08-02 12:09:58 -0500

    Added roles, users, and not authorized view. Changed all routes, methods, and publications to authenticate user if necessary.

--- server/publications.js
+++ server/publications.js
@@ -1,17 +1,41 @@
-Meteor.publish('pages', function() {
-  return Pages.find();
-});
-
-Meteor.publish('page', function(id){
-  check(id, String);
-  return Pages.find({"_id": id});
-});
-
 Meteor.publish('find-page', function(title){
   check(title, String);
   return Pages.find({"title": title});
 });
 
+Meteor.publish('pages', function() {
+  if (Roles.userIsInRole(this.userId, ['admin'])){
+    return Pages.find();
+  } else {
+    this.stop();
+    return;
+  }
+});
+
+Meteor.publish('page', function(id){
+  if (Roles.userIsInRole(this.userId, ['admin'])){
+    check(id, String);
+    return Pages.find({"_id": id});
+  } else {
+    this.stop();
+    return;
+  }
+});
+
 Meteor.publish('analytics', function(){
-  return Analytics.find();
+  if (Roles.userIsInRole(this.userId, ['admin'])){
+    return Analytics.find();
+  } else {
+    this.stop();
+    return;
+  }
+});
+
+Meteor.publish('all-users', function() {
+  if (Roles.userIsInRole(this.userId, ['admin'])){
+    return Meteor.users.find();
+  } else {
+    this.stop();
+    return;
+  }
 });
